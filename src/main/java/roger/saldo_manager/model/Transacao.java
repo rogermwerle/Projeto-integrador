24d0bc8da01571cac394cb5007e5b56a151ceea7
Roger Marques <roger.m.werle@gmail.com>
2025-01-05 10:25:36 -0300

    Criação das entidades

---
+++ src/main/java/roger/saldo_manager/model/Transacao.java
@@ -0,0 +1,38 @@
+package roger.saldo_manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "transacoes")
+class Transacao {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private LocalDateTime dataHora;
+    private Double valor;
+    private String tipo;
+    
+    @ManyToOne
+    @JoinColumn(name = "remetente_id", nullable = false)
+    private Usuario remetente;
+    
+    @ManyToOne
+    @JoinColumn(name = "destinatario_id", nullable = false)
+    private Usuario destinatario;
+    private String descrição;
+}
---
+++ src/main/java/roger/saldo_manager/model/Usuario.java
@@ -0,0 +1,26 @@
+package roger.saldo_manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+    private Double saldo;
+
+}
