5f00dc563f56e258f1cd652827cf6121d3c4fb9e
Roger Marques <roger.m.werle@gmail.com>
2025-01-05 04:24:47 -0300

    Criação do Projeto

---
+++ src/main/java/roger/saldo_manager/SaldoManagerApplication.java
@@ -0,0 +1,13 @@
+package roger.saldo_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaldoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaldoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/roger/saldo_manager/SaldoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package roger.saldo_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaldoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
